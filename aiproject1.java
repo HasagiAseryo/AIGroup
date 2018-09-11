fbd270f8da235c03bd1c568d89f08b18abe353ac
Matthew Pointer <mpointer@amctechnology.com>
2018-09-11 16:03:59 -0400

    first push

---
+++ aiproject1.java
@@ -0,0 +1,26 @@
+import java.lang.Math;
+import java.util.Random;
+public class Main{
+
+    public static void main(String[] args){
+
+        //Define main data variables
+        public double height;
+        public double weight;
+        public int gender;
+
+        FileWriter fileWriter = new FileWriter("data.txt"); //Define output file
+        PrintWriter printWriter = new PrintWriter(fileWriter); //Set up printer to file. 
+
+        //Generate males
+        
+        public int minFeet;
+        for(int i = 0; i < 2000; i++) {
+            public int feetRange = (Math.abs(Random.nextGaussian()*5))+3; //Get normally dist. height between 3-8ft.
+            System.out.println(feetRange);
+            public int inches;
+
+
+        }
+    }
+}
